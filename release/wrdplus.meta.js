53595c4f2ca2ea122f6ce48ac0f855b72f3cf144
infinity-script <39848886+infinity-Script@users.noreply.github.com>
2020-09-14 19:36:16 -0400

    Modified "release"

--- release/wrdplus.meta.js
+++ release/wrdplus.meta.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name WRD+
 // @author SoundInfinity
-// @version 3.1.1
+// @version 3.1.2
 // @updateURL https://rawcdn.githack.com/infinity-Script/wrdplus/0e7fd015f224aa3f4a1670d50bc2fc95e7479ea1/master/wrdplus.user.js
 // @downloadURL https://rawcdn.githack.com/infinity-Script/wrdplus/0e7fd015f224aa3f4a1670d50bc2fc95e7479ea1/master/wrdplus.user.js
 // @include http*://wearedevs.net/*
