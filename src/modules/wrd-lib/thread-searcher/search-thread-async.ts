31adbfdc4c5850b7f9748ee8030f7d08749cbfb5
infinity-script <39848886+infinity-Script@users.noreply.github.com>
2020-09-14 17:13:15 -0400

    Updated thread lookup function. (indivual one)

--- src/modules/wrd-lib/thread-searcher/search-thread-async.ts
+++ src/modules/wrd-lib/thread-searcher/search-thread-async.ts
@@ -1,12 +1,7 @@
 import { QueryEndpoint, GetThreadsFromBodyHTML } from './utils'
 import { SearchResults } from '../classes'
 
-export function searchThreadAsync(threadName: string): Promise<SearchResults> {
-    return new Promise(resolve => {
-        fetch(QueryEndpoint.replace("$name", threadName))
-            .then(res => res.text())
-            .then(body => {
-                resolve(GetThreadsFromBodyHTML(body))
-            })
-    })
+export async function searchThreadAsync(threadName: string): Promise<SearchResults> {
+    const body: string = await fetch(QueryEndpoint.replace('$name', threadName)).then(res => res.text())
+    return GetThreadsFromBodyHTML(body)
 }
