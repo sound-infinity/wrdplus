80fcc153faa69c1042ab2496af3023c4cbd8f10e
SoundInfinity <39848886+infinity-Script@users.noreply.github.com>
2020-12-06 00:15:43 -0500

    Added dark & light theme for popup links

--- src/modules/wrd-lib/style-sheets/popup/index.js
+++ src/modules/wrd-lib/style-sheets/popup/index.js
@@ -1,5 +1,5 @@
 import General from './general.style.css'
-//import Light from './light.style.css'
-//import Dark from './dark.style.css'
+import Light from './light.style.css'
+import Dark from './dark.style.css'
 
-export default { General }
+export default { General, Dark, Light }
