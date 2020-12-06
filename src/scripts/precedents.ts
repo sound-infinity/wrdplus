24ae9be8f4562edcfa0d93e1d5cb006551b3f705
SoundInfinity <39848886+infinity-Script@users.noreply.github.com>
2020-12-06 00:36:16 -0500

    Added startup file

---
+++ src/scripts/precedents.ts
@@ -0,0 +1,19 @@
+// Code that runs before any other script to setup items before they are used.
+// ##########
+// Should not use external sources
+
+import { LinkType } from "../modules/wrd-lib/enums/link-type"
+import { getLinkType } from "../modules/wrd-lib/utils/page"
+import { ExtraFeatures } from "./settings"
+
+//#region ThreadAnchors
+if (getLinkType() === LinkType.Section || getLinkType() === LinkType.Index) {
+    if (ExtraFeatures.get<boolean>("threadmarkings")) {
+        const threadAnchors = document.querySelectorAll<HTMLAnchorElement>('a[href*="/forum/t"')
+        for (let i=0;i<threadAnchors.length;++i) {
+            const thread = threadAnchors[i]
+            thread.setAttribute("state", "Waiting")
+        }    
+    }
+}
+//#endregion ThreadAnchors
