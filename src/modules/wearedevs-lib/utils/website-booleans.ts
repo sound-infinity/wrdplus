4dff4c03b5c1b2e6a27bebb51dceb7d356a079a2
SoundInfinity <39848886+sound-infinity@users.noreply.github.com>
2021-12-01 23:13:35 -0500

    Script for detecting theme rewritten

--- src/modules/wearedevs-lib/utils/website-booleans.ts
+++
@@ -1,7 +0,0 @@
-export function isLightModeActive() {
-    const element = document.querySelector<HTMLInputElement>("#themer")
-    if (element != null) {
-        return element.checked
-    } else {
-    }
-}
