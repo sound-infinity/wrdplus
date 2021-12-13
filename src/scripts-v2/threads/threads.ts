1d3bd1a730db7cea03edb671237e10bb615cf0ed
SoundInfinity <39848886+sound-infinity@users.noreply.github.com>
2021-12-13 15:05:06 -0500

    better thread tags

--- src/scripts-v2/threads/threads.ts
+++ src/scripts-v2/threads/threads.ts
@@ -6,10 +6,10 @@ function title_element(title: string, tag: string) {
     template.innerHTML = HtmlThreadTitleTag
     const element = template?.firstElementChild
     if (element != null) {
+        // TitleTag
         const titleTag = element?.querySelector(".thread-title-tag")
-        if (titleTag != null && titleTag.textContent != null) titleTag.textContent = tag
-    }
-    if (element != null) {
+        if (titleTag != null && titleTag.textContent != null) titleTag.textContent = tag.trim()
+        // TitleText
         const titleText = element?.querySelector(".thread-title-text")
         if (titleText != null && titleText.textContent != null) titleText.textContent = title
     }
@@ -32,34 +32,50 @@ function parse_title(title: string) {
     return parsed
 }
 
-easyLoad.getSavedValue(DB.DB_FEATURES, "thread_highlights").then((value) => {
-    if (value === true) {
-        let threadTitles = document.querySelector("table>tbody")?.querySelectorAll("a[href*=t].thread-title")
+function load_thread_highlights() {
+    // if (value === true) {
+    let threadTitles = document.querySelector("table>tbody")?.querySelectorAll("a[href*=t].thread-title")
 
-        function alternative() {
-            threadTitles = document.querySelectorAll('a[href*="forum/t/"]:not([class*=btn])')
-        }
+    function alternative() {
+        threadTitles = document.querySelectorAll('a[href*="forum/t/"]:not([class*=btn])')
+    }
 
-        function alternative_2() {
-            threadTitles = document.querySelectorAll("#topic")
-        }
+    function alternative_2() {
+        threadTitles = document.querySelectorAll("#topic")
+    }
 
-        if (threadTitles == null || threadTitles.length < 1) alternative()
-        if (threadTitles == null || threadTitles.length < 1) alternative_2()
-        if (threadTitles == null) return
+    if (threadTitles == null || threadTitles.length < 1) alternative()
+    if (threadTitles == null || threadTitles.length < 1) alternative_2()
+    if (threadTitles == null) return
 
-        for (const threadTitle of Object.values(threadTitles)) {
-            if (threadTitle.textContent != null) {
-                const parsedThreadTitle = parse_title(threadTitle.textContent)
-                if (parsedThreadTitle.title != null && parsedThreadTitle.tag != null) {
-                    const tag_Element = title_element(parsedThreadTitle.title, parsedThreadTitle.tag)
-                    if (tag_Element && threadTitle.parentNode) {
-                        threadTitle.parentNode.insertBefore(tag_Element, threadTitle)
-                        if (threadTitle.getAttribute("href") != null) tag_Element.setAttribute("href", threadTitle.getAttribute("href") || "")
-                        threadTitle.remove()
-                    }
+    for (const threadTitle of Object.values(threadTitles)) {
+        if (threadTitle.textContent != null) {
+            const parsedThreadTitle = parse_title(threadTitle.textContent)
+            if (parsedThreadTitle.title != null && parsedThreadTitle.tag != null) {
+                const tag_Element = title_element(parsedThreadTitle.title, parsedThreadTitle.tag)
+                if (tag_Element && threadTitle.parentNode) {
+                    threadTitle.parentNode.insertBefore(tag_Element, threadTitle)
+                    if (threadTitle.getAttribute("href") != null) tag_Element.setAttribute("href", threadTitle.getAttribute("href") || "")
+                    threadTitle.remove()
                 }
             }
+        }
+    }
+    // }
+}
+
+easyLoad.getSavedValue(DB.DB_FEATURES, "thread_highlights").then((value) => {
+    if (value === true) {
+        switch (document.readyState) {
+            case "complete":
+            case "interactive":
+                load_thread_highlights()
+                break
+            default:
+                document.addEventListener("readystatechange", () => {
+                    if (document.readyState === "interactive") load_thread_highlights()
+                })
+                break
         }
     }
 })
