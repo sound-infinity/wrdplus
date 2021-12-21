10e6d448e79e6cad76b5ed38731d2ae11bddc3df
SoundInfinity <39848886+sound-infinity@users.noreply.github.com>
2021-12-21 01:17:56 -0500

    @updateUrl -> @updateURL

--- release/build/meta.js
+++ release/build/meta.js
@@ -4,7 +4,7 @@
 // @author        SoundInfinity
 // @version       4.6.1
 // @run-at        document-start
-// @updateUrl     https://sound-infinity.github.io/wrdplus/release/wrdplus.user.js
+// @updateURL     https://sound-infinity.github.io/wrdplus/release/wrdplus.user.js
 // @downloadURL   https://sound-infinity.github.io/wrdplus/release/wrdplus.user.js
 // @homepage      https://github.com/infinity-Script/wrdplus
 // @include       http*://wearedevs.net/*
