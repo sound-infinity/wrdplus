b57626f63651066374e22fb7b3609ce07616da61
SoundInfinity <39848886+sound-infinity@users.noreply.github.com>
2021-12-21 01:03:59 -0500

    Built with Better Mentions

--- release/build/meta.js
+++ release/build/meta.js
@@ -2,8 +2,10 @@
 // ==UserScript==
 // @name          WRD+
 // @author        SoundInfinity
-// @version       4.5.0
+// @version       4.6.0
 // @run-at        document-start
+// @updateUrl     https://sound-infinity.github.io/wrdplus/release/wrdplus.user.js
+// @downloadURL   https://sound-infinity.github.io/wrdplus/release/wrdplus.user.js
 // @homepage      https://github.com/infinity-Script/wrdplus
 // @include       http*://wearedevs.net/*
 // ==/UserScript==
