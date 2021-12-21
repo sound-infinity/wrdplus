56afd1ce2c53af47436048def428755b4205be56
SoundInfinity <39848886+sound-infinity@users.noreply.github.com>
2021-12-21 01:15:42 -0500

    patched themer > custom-logo

--- src/scripts/themer/custom-logo/index.ts
+++ src/scripts/themer/custom-logo/index.ts
@@ -1,11 +1,15 @@
+import { DB_THEME_MODIFICATIONS } from "../../settings/constants"
+import selectStorage from "../../settings/easy-load"
 import { parseHTML } from "../utils"
 import HtmlCustomLogo from "./custom-logo.html"
 
 export function initialize() {
-    const logo_mod = <HTMLAnchorElement>parseHTML(HtmlCustomLogo)
-    const logo_original = document.querySelector("#foologo")
-    logo_original?.parentElement?.insertBefore(logo_mod, logo_original)
-    logo_original?.remove()
+    if (selectStorage(DB_THEME_MODIFICATIONS)("wearedevs_logo_asImage") === true) {
+        const logo_mod = <HTMLAnchorElement>parseHTML(HtmlCustomLogo)
+        const logo_original = document.querySelector("#foologo")
+        logo_original?.parentElement?.insertBefore(logo_mod, logo_original)
+        logo_original?.remove()
+    }
 
     // if (logo_mod != null) {
     //     const icon = document.createElement("img")
